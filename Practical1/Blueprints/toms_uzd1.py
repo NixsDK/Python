62294926a7957cda0bfb84be3fd5f72de866d62b
TomGHcode <82314188+TomGHcode@users.noreply.github.com>
2024-02-28 01:30:48 +0200

    Create toms_uzd1.py

---
+++ Practical1/Blueprints/toms_uzd1.py
@@ -0,0 +1,10 @@
+work_hours = int(input("Please input your work hours: "))
+pay_rate = float(input("Please input your pay rate: "))
+
+if work_hours > 40:
+    gross_pay = ((work_hours - 40) * pay_rate * 1.25) + 40 * pay_rate
+    print("Your pay is: ", gross_pay, "(gross)")
+
+else:
+    gross_pay = work_hours * pay_rate
+    print("Your pay is: ", gross_pay, "(gross)")
