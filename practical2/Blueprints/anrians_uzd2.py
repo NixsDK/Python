5cb1338d673dc728ebcce63df9389bcec13d67e8
Anrians Fedotovs <156031292+Andzhaa@users.noreply.github.com>
2024-03-11 22:10:25 +0200

    izveidoju mapes un ieliku failu

---
+++ practical2/Blueprints/anrians_uzd2.py
@@ -0,0 +1,25 @@
+def compute_wage (hours, pay_rate):
+    pay=0
+    if hours >= 40:
+        pay=pay_rate * 40 + (hours - 40) * pay_rate * 1.25
+    else:
+        pay=hours*pay_rate
+    return pay
+
+
+while True:
+    try:
+        input_hours = int(input('Input your work hours: '))
+        break
+    except:
+        print('Please enter numeric value!\n')
+
+while True:
+    try:
+        input_pay_rate = float(input('Input your pay rate: '))
+        break
+    except:
+        print('Please enter numeric value!')
+
+
+print('Your salary is ', compute_wage(input_hours, input_pay_rate))
