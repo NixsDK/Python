7e6200f794ed49cce2867eeb4b65e15ce6849a87
TomGHcode <82314188+TomGHcode@users.noreply.github.com>
2024-03-13 02:32:20 +0200

    Add files via upload

---
+++ practical2/Blueprints/toms_uzd1.py
@@ -0,0 +1,24 @@
+def compute_wage (work_hours, pay_rate):
+    gross_pay = 0
+    if work_hours > 40:
+        gross_pay = ((work_hours - 40) * pay_rate * 1.25) + 40 * pay_rate
+
+    else:
+        gross_pay = work_hours * pay_rate
+    return gross_pay
+
+while True:
+    try:
+        work_hours = int(input("Please input your work hours: "))
+        break
+    except:
+        print('Please enter a numeric value! \n')
+while True:
+    try:
+        pay_rate = float(input("Please input your pay rate: "))
+        break
+    except:
+        print('Please enter a numeric value! \n')
+
+
+print('Your pay is ', compute_wage(work_hours, pay_rate), "(gross)")
