0e05fecd5a9a8c3b0af09e4e1bcb960cb9397c89
Anrians Fedotovs <156031292+Andzhaa@users.noreply.github.com>
2024-04-22 23:35:34 +0300

    ieliku savu blueprintu un izvedioju mapi prieks viniem

---
+++ Practical4/Blueprint/emails1_Anrians.py
@@ -0,0 +1,20 @@
+import re
+
+file_name = input("Input filename: ")
+
+file = open(file_name)
+search_pattern = r'(^From+.*\d)'
+email_sources = list()
+
+for line in file:
+
+    columns = line.strip().split(" ")
+    if re.search(search_pattern, line):
+        email_sources.append(columns[1])
+
+email_sources.sort()
+
+for emails in email_sources:
+    print(emails)
+
+print("\nThere were ", len(email_sources), " lines that start with 'From'")
