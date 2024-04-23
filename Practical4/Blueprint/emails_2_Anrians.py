7517d483ed6f6f1f77e65cb5a906f19701841144
Anrians Fedotovs <156031292+Andzhaa@users.noreply.github.com>
2024-04-23 23:51:21 +0300

    iemetu blueprint 2 uzdevumam

---
+++ Practical4/Blueprint/emails_2_Anrians.py
@@ -0,0 +1,28 @@
+import re
+
+file_name = input("Input filename: ")
+
+file = open(file_name)
+search_pattern = r'(^From+.+\d)'
+email_sources = {}
+
+for line in file:
+
+    columns = line.strip().split(" ")
+    if re.search(search_pattern, line):
+        domains = columns[1].split("@")
+        email_sources[domains[1]] = email_sources.get(domains[1], 0)  + 1
+
+print(email_sources, "\n")
+
+sorted = sorted(email_sources.items(), key=lambda item: item[1])
+sorted_emails = dict(sorted)
+
+longest_key = max(len(key) for key in sorted_emails.keys())
+longest_value = max(len(str(value)) for value in sorted_emails.values())
+
+print("SORTED:")
+for domains,ammount in sorted_emails.items():
+    key_str = domains.rjust(longest_key)
+    value_str = str(ammount).rjust(longest_value)
+    print(f"{key_str}: {value_str}", "*"*ammount)
