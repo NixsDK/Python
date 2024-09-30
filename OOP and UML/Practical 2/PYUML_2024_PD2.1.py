7eca016013b3711a8d84f2773f815127a42f2fc8
Anrians Fedotovs <156031292+Andzhaa@users.noreply.github.com>
2024-09-30 23:20:47 +0300

    Create PYUML_2024_PD2.1.py

---
+++ OOP and UML/Practical 2/PYUML_2024_PD2.1.py
@@ -0,0 +1,20 @@
+import turtle
+
+def draw_star(sides, length):
+  
+    if sides % 2 == 0:
+        print("Error: n must be an odd number.")
+        return
+    
+    angle = 180 - (180 / sides)
+    
+    for _ in range(sides):
+        turtle.forward(length)
+        turtle.right(angle)
+
+print("Enter the number of sides and length of the star")
+x=int(input("sides->" ))
+y=int(input("length->" ))
+turtle.speed(5)
+draw_star(x, y)
+turtle.done()
